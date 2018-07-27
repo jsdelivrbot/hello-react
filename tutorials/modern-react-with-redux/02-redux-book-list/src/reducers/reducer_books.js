49a2e7817e7f27a808ddaabcbc8f85a38f5feb3e
Nico Serfontein <nserfontein@gmail.com>
2018-07-27 12:23:17 +0200

    Update

---
+++ tutorials/modern-react-with-redux/02-redux-book-list/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title : 'Javascript: The Good Parts', pages: 11},
+        { title : 'Harry Potter', pages: 12},
+        { title : 'The Dark Tower', pages: 13},
+        { title : 'Eloquent Ruby', pages: 14}
+    ]
+}
